d3b3c9f4b818a18a72f1990d5a0d50d91e2ece75
Fabrizio Piñánez <pinhaneez1992@gmail.com>
2026-03-11 17:26:16 -0300

    mejora del codigo

--- trivia.py
+++ trivia.py
@@ -27,3 +27,22 @@ nombre = input('dime tu nombre: ')
 print('bienvenido a esta trivia', nombre) 
 preguntas = ['cual es la capital de paraguay?', 'cual es la capital de argentina?', 'cual es la capital de brasil?', 'cual es la capital de uruguay?']
 
+suma = 0
+respuesta1 = input(preguntas[0])
+if respuesta1 == 'asuncion':
+    suma += 1
+respuesta2 = input(preguntas[1])
+if respuesta2 == 'buenos aires':
+    suma += 1
+respuesta3 = input(preguntas[2])
+if respuesta3 == 'brasilia':
+    suma += 1
+respuesta4 = input(preguntas[3])
+if respuesta4 == 'montevideo':
+    suma += 1
+if suma == 4: 
+    print('Excelente')
+elif suma >= 2:
+    print('Muy bien')
+else:
+    print('Puedes mejorar')
