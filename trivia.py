1992f74a6f81e6ac13a7b99db28fdf7bd71a21d8
Fabrizio Piñánez <pinhaneez1992@gmail.com>
2026-03-11 17:24:42 -0300

    este es el inicio de mi codigo

---
+++ trivia.py
@@ -0,0 +1,29 @@
+'''# trivia
+# es juego de triviaaa!
+# Challenge: Mini Trivia en Python
+
+## Debes crear:
+Un archivo llamado `trivia.py`
+
+## Tu programa debe:
+- pedir el nombre del jugador
+- mostrar una bienvenida
+- hacer 4 preguntas
+- sumar 1 punto por cada respuesta correcta
+- mostrar el nombre y el puntaje final
+
+## Resultado final
+- si `puntaje == 4` → **Excelente**
+- si `puntaje >= 2` → **Muy bien**
+- si no → **Puedes mejorar**
+
+## Recuerda
+- trabaja paso a paso
+- no hace falta terminar perfecto
+- usa Git durante el proceso
+- haz varios commits pequeños '''
+
+nombre = input('dime tu nombre: ')
+print('bienvenido a esta trivia', nombre) 
+preguntas = ['cual es la capital de paraguay?', 'cual es la capital de argentina?', 'cual es la capital de brasil?', 'cual es la capital de uruguay?']
+
